52098dcae1eeab1f96e50ebace0f7f5b6ef8dfb5
Sanjay Bikhchandani <sanjay.bikhchandani@tatvasoft.com>
2023-02-22 15:09:28 +0530

    Initial Commit

---
+++ src/ckeditor.js
@@ -0,0 +1,52 @@
+/**
+ * @license Copyright (c) 2014-2023, CKSource Holding sp. z o.o. All rights reserved.
+ * For licensing, see LICENSE.md or https://ckeditor.com/legal/ckeditor-oss-license
+ */
+import ClassicEditor from '@ckeditor/ckeditor5-editor-classic/src/classiceditor.js';
+import Bold from '@ckeditor/ckeditor5-basic-styles/src/bold.js';
+import Essentials from '@ckeditor/ckeditor5-essentials/src/essentials.js';
+import Italic from '@ckeditor/ckeditor5-basic-styles/src/italic.js';
+import Link from '@ckeditor/ckeditor5-link/src/link.js';
+import List from '@ckeditor/ckeditor5-list/src/list.js';
+import ListProperties from '@ckeditor/ckeditor5-list/src/listproperties.js';
+import Paragraph from '@ckeditor/ckeditor5-paragraph/src/paragraph.js';
+import RemoveFormat from '@ckeditor/ckeditor5-remove-format/src/removeformat.js';
+import Underline from '@ckeditor/ckeditor5-basic-styles/src/underline.js';
+
+class Editor extends ClassicEditor {}
+
+// Plugins to include in the build.
+Editor.builtinPlugins = [
+	Bold,
+	Essentials,
+	Italic,
+	Link,
+	List,
+	ListProperties,
+	Paragraph,
+	RemoveFormat,
+	Underline
+];
+
+// Editor configuration.
+Editor.defaultConfig = {
+	toolbar: {
+		items: [
+			'bold',
+			'underline',
+			'italic',
+			'|',
+			'bulletedList',
+			'numberedList',
+			'|',
+			'link',
+			'removeFormat',
+			'|',
+			'undo',
+			'redo'
+		]
+	},
+	language: 'en-gb'
+};
+
+export default Editor;
